c409b7e9d3533ff165720d44fbbca4f6326e73ab
ella <ella@choeja-eun-ui-MacBookAir.local>
2022-05-15 14:16:55 +0900

    create pages and setting error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import { Switch, BrowserRouter, Router } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Add from './pages/Add';
+import DetailPage from './pages/DetailPage'; 
+import Edit from './pages/Edit';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Signin from './pages/Signin';
+import Error from './pages/Error';
 
-function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Router />
-      </Switch>
-    </BrowserRouter>
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/book/:id" element={<DetailPage />} />
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   )
 }
 
